9fd4cc6f1b0ca4e3559500edc72d1b13c502d9f5
Ewan <ewanwd2008@gmail.com>
2026-05-22 21:40:53 +0100

    Enemies spawning

--- index.js
+++ index.js
@@ -45,6 +45,29 @@ class Projectile {
     }
 }
 
+class Enemy{
+    constructor(x,y,radius,colour,velocity) {
+        this.x=x
+        this.y=y
+        this.radius=radius
+        this.colour=colour
+        this.velocity=velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        c.fillStyle = this.colour
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 const x = canvas.width/2
 const y = canvas.height/2
 
@@ -52,6 +75,38 @@ const player = new Player(x,y, 30, "blue")
 
 
 const projectiles = []
+const enemies = []
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (60 - 15) + 15
+
+        let x
+        let y
+
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+
+        const colour = "green"
+        const angle = Math.atan2(
+            -y+canvas.height/2,
+            -x+canvas.width/2)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        enemies.push(new Enemy(x,y,radius,colour,velocity))
+    }, 1000)
+
+}
+
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
@@ -62,6 +117,9 @@ function animate() {
         }
     )
 
+    enemies.forEach((enemy) => {
+        enemy.update()
+    })
 }
 
 addEventListener('click', (event) => 
@@ -81,3 +139,4 @@ addEventListener('click', (event) =>
 
 
 animate()
+spawnEnemies() 
