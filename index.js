a323b8db760cb71d37cf0b21d76e6d59ce2d38bd
Ewan <ewanwd2008@gmail.com>
2026-05-22 21:54:53 +0100

    Collisons between projectiles and enemies

--- index.js
+++ index.js
@@ -117,8 +117,21 @@ function animate() {
         }
     )
 
-    enemies.forEach((enemy) => {
+    enemies.forEach((enemy, index) => {
         enemy.update()
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+            if (dist - enemy.radius - projectile.radius < 1)
+            {
+                setTimeout(() => {
+                    enemies.splice(index, 1)
+                    projectiles.splice(projectileIndex, 1)
+                }, 0)
+
+            }
+        })
     })
 }
 
